bf26d4717f1b932d52994ad1b6ecee6d4f32b85d
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-04-01 09:34:07 +0200

    project, issue

---
+++ src/main/java/wsb/bugtracker/models/Issue.java
@@ -0,0 +1,51 @@
+package wsb.bugtracker.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Issue {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.TODO;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Priority priority = Priority.NORMAL;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Type type = Type.TASK;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id", nullable = false)
+    private Person creator;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "assignee_id")
+    private Person assignee;
+
+    @Column(nullable = false)
+    private Date dateCreated;
+
+    @Column(nullable = false)
+    private Date lastUpdated;
+}
