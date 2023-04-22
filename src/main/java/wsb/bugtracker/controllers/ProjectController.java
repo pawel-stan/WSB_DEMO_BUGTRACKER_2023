e1a1fa2c18a9690ec4b3e69385c061b019d166f0
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-04-22 11:04:46 +0200

    - zaciąganie ludzi do filtra po twórcy

--- src/main/java/wsb/bugtracker/controllers/ProjectController.java
+++ src/main/java/wsb/bugtracker/controllers/ProjectController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import wsb.bugtracker.models.Person;
 import wsb.bugtracker.models.Project;
+import wsb.bugtracker.services.PersonService;
 import wsb.bugtracker.services.ProjectService;
 
 import java.util.List;
@@ -14,14 +16,16 @@ import java.util.List;
 @RequestMapping("/projects")
 @RequiredArgsConstructor
 public class ProjectController {
-
     final private ProjectService projectService;
+    final private PersonService personService;
 
     @GetMapping
     ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("projects/index");
         List<Project> projects = projectService.findAll();
         modelAndView.addObject("projects", projects);
+        List<Person> people = personService.findAll();
+        modelAndView.addObject("people", people);
         return modelAndView;
     }
 
