a426582466c9ae770255aa91d4b190edeca7569b
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-04-22 11:19:09 +0200

    - wysyłanie wartości wpisanych w filtrze do kontrolera

--- src/main/java/wsb/bugtracker/controllers/ProjectController.java
+++ src/main/java/wsb/bugtracker/controllers/ProjectController.java
@@ -3,8 +3,10 @@ package wsb.bugtracker.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import wsb.bugtracker.filters.ProjectFilter;
 import wsb.bugtracker.models.Person;
 import wsb.bugtracker.models.Project;
 import wsb.bugtracker.services.PersonService;
@@ -20,12 +22,13 @@ public class ProjectController {
     final private PersonService personService;
 
     @GetMapping
-    ModelAndView index() {
+    ModelAndView index(@ModelAttribute ProjectFilter filter) {
         ModelAndView modelAndView = new ModelAndView("projects/index");
         List<Project> projects = projectService.findAll();
         modelAndView.addObject("projects", projects);
         List<Person> people = personService.findAll();
         modelAndView.addObject("people", people);
+        modelAndView.addObject("filter", filter);
         return modelAndView;
     }
 
