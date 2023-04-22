09e46eb703e20e72a2fa102ed52dbdb3337969bc
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-04-22 10:35:36 +0200

    - wstęp do filtrowania

--- src/main/java/wsb/bugtracker/repositories/ProjectRepository.java
+++ src/main/java/wsb/bugtracker/repositories/ProjectRepository.java
@@ -1,9 +1,21 @@
 package wsb.bugtracker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import wsb.bugtracker.models.Project;
 
+import java.util.List;
+
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
+
+    @Query(value = "select * from project where enabled = :enabled",
+            nativeQuery = true)
+    List<Project> findAllByEnabledNative(@Param("enabled") boolean enabled);
+
+    List<Project> findAllByEnabled(boolean enabled);
+
 }
--- src/main/java/wsb/bugtracker/services/ProjectService.java
+++ src/main/java/wsb/bugtracker/services/ProjectService.java
@@ -1,6 +1,7 @@
 package wsb.bugtracker.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import wsb.bugtracker.models.Project;
 import wsb.bugtracker.repositories.ProjectRepository;
@@ -13,6 +14,10 @@ public class ProjectService {
     final private ProjectRepository projectRepository;
 
     public List<Project> findAll() {
-        return projectRepository.findAll();
+
+        Specification<Project> isEnabled = (root, query, builder) -> builder.equal(root.get("enabled"), true);
+        Specification<Project> nameIlikeProject = (root, query, builder) -> builder.like(builder.lower(root.get("name")), "%projekt%");
+
+        return projectRepository.findAll(isEnabled.and(nameIlikeProject));
     }
 }
