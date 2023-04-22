f2037e09b2f4b91ee859b2a50cfadf0cfe661d52
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-04-22 10:41:47 +0200

    - wstęp do filtrowania

--- src/main/java/wsb/bugtracker/services/ProjectService.java
+++ src/main/java/wsb/bugtracker/services/ProjectService.java
@@ -3,6 +3,7 @@ package wsb.bugtracker.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import wsb.bugtracker.models.Person;
 import wsb.bugtracker.models.Project;
 import wsb.bugtracker.repositories.ProjectRepository;
 
@@ -17,7 +18,8 @@ public class ProjectService {
 
         Specification<Project> isEnabled = (root, query, builder) -> builder.equal(root.get("enabled"), true);
         Specification<Project> nameIlikeProject = (root, query, builder) -> builder.like(builder.lower(root.get("name")), "%projekt%");
+        Specification<Project> creatorNameEqual = (root, query, builder) -> builder.equal(root.get("creator").get("realName"), "Kasia D");
 
-        return projectRepository.findAll(isEnabled.and(nameIlikeProject));
+        return projectRepository.findAll(isEnabled.and(nameIlikeProject).and(creatorNameEqual));
     }
 }
