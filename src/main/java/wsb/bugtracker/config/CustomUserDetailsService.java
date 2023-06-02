e9ab77ab503e51eae20b6aa479149ccd2cabc952
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2023-06-02 11:36:41 +0200

    Obsługa logowania na użytkownika w bazie

---
+++ src/main/java/wsb/bugtracker/config/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package wsb.bugtracker.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import wsb.bugtracker.models.Person;
+import wsb.bugtracker.repositories.PersonRepository;
+
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final PersonRepository personRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Person person = personRepository.findByUsername(username).orElse(null);
+
+        if (person == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new User(username, person.getPassword(), Collections.emptyList());
+    }
+}
--- src/main/java/wsb/bugtracker/config/SecurityConfig.java
+++ src/main/java/wsb/bugtracker/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package wsb.bugtracker.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -30,5 +35,16 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(bCryptPasswordEncoder());
+
+        provider.setUserDetailsService(customUserDetailsService);
+
+        return provider;
     }
 }
