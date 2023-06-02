991cf996a3bad030b31da127d3b8c790fb3b51a6
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2023-06-02 13:09:42 +0200

    Zapisuje wszystkie brakujące uprawnienia w bazie i do użytkownika admin

--- src/main/java/wsb/bugtracker/config/Bootstrap.java
+++ src/main/java/wsb/bugtracker/config/Bootstrap.java
@@ -3,14 +3,20 @@ package wsb.bugtracker.config;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
+import wsb.bugtracker.models.Authority;
 import wsb.bugtracker.models.AuthorityName;
+import wsb.bugtracker.repositories.AuthorityRepository;
 import wsb.bugtracker.services.PersonService;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class Bootstrap implements InitializingBean {
 
     private PersonService personService;
+    private AuthorityRepository authorityRepository;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -23,7 +29,18 @@ public class Bootstrap implements InitializingBean {
 
     void saveMissingAuthorities() {
         for (AuthorityName authorityName : AuthorityName.values()) {
-            // TODO: Sprawdź, czy istnieje Authority o takiej nazwie i jeśli nie, to stwórz nowe
+            Optional<Authority> authority = authorityRepository.findByName(authorityName);
+
+            if (authority.isPresent()) {
+                continue;
+            }
+
+            Authority newAuthority = new Authority();
+            newAuthority.setName(authorityName);
+
+            System.out.println("Zapisujemy nowe uprawnienie: " + authorityName.name());
+
+            authorityRepository.save(newAuthority);
         }
     }
 }
--- src/main/java/wsb/bugtracker/services/PersonService.java
+++ src/main/java/wsb/bugtracker/services/PersonService.java
@@ -3,11 +3,15 @@ package wsb.bugtracker.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import wsb.bugtracker.models.Authority;
 import wsb.bugtracker.models.Person;
+import wsb.bugtracker.repositories.AuthorityRepository;
 import wsb.bugtracker.repositories.PersonRepository;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,7 @@ public class PersonService {
 
     final private PersonRepository personRepository;
     final private BCryptPasswordEncoder bCryptPasswordEncoder;
+    final private AuthorityRepository authorityRepository;
 
     public List<Person> findAll() {
         return personRepository.findAll();
@@ -32,6 +37,7 @@ public class PersonService {
 
         if (person.isPresent()) {
             System.out.println("Użytkownik administracyjny już istnieje, przerywamy");
+            saveAllAuthorities(person.get());
             return;
         }
 
@@ -45,10 +51,16 @@ public class PersonService {
         newPerson.setPassword(hashedPassword);
 
         personRepository.save(newPerson);
+
+        saveAllAuthorities(newPerson);
     }
 
     public void saveAllAuthorities(Person person) {
-        // Znajdź wszystkie istniejące w bazie Authority
-        // Dodaj użytkownikowi person wpis w PersonAuthorities, jeżeli jeszcze takiego nie ma dla danego uprawnienia
+        List<Authority> authorities = authorityRepository.findAll();
+        Set<Authority> authoritySet = new HashSet<>(authorities);
+
+        person.setAuthorities(authoritySet);
+
+        personRepository.save(person);
     }
 }
