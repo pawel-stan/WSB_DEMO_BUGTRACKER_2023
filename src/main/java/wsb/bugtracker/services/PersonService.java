1607e81758b71333752f29b63a7f138fc43ada9c
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2023-06-02 10:55:36 +0200

    Zapisywanie użytkownika administracyjnego

--- src/main/java/wsb/bugtracker/services/PersonService.java
+++ src/main/java/wsb/bugtracker/services/PersonService.java
@@ -1,17 +1,20 @@
 package wsb.bugtracker.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import wsb.bugtracker.models.Person;
 import wsb.bugtracker.repositories.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PersonService {
 
     final private PersonRepository personRepository;
+    final private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public List<Person> findAll() {
         return personRepository.findAll();
@@ -19,5 +22,28 @@ public class PersonService {
 
     public void delete(Long id) {
         personRepository.deleteById(id);
+    }
+
+    public void saveAdmin() {
+        String username = "admin";
+        String password = "aA123456";
+
+        Optional<Person> person = personRepository.findByUsername(username);
+
+        if (person.isPresent()) {
+            System.out.println("Użytkownik administracyjny już istnieje, przerywamy");
+            return;
+        }
+
+        System.out.println("Tworzymy użytkownika administracyjnego...");
+
+        Person newPerson = new Person();
+        newPerson.setUsername(username);
+        newPerson.setRealName(username);
+
+        String hashedPassword = bCryptPasswordEncoder.encode(password);
+        newPerson.setPassword(hashedPassword);
+
+        personRepository.save(newPerson);
     }
 }
